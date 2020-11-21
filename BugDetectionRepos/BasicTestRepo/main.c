404741029420cfa138360178c5445a129dca5026
Florian Sattler <vuld3r@gmail.com>
2020-11-21 20:06:38 +0100

    Fixes wrongly generated bug repo

--- BugDetectionRepos/BasicTestRepo/main.c
+++ BugDetectionRepos/BasicTestRepo/main.c
@@ -2,28 +2,12 @@
 
 int sum(int a, int b) { return a - b; }
 
-#include <stdio.h>
-
-int sum(int a, int b) { return a - b; }
-
 int multiply(int a, int b) {
-return local;
-}
-
-int multiply(int a, int b) {
-// computed correct result!
-int local = a * b;
 // computed correct result!
 int local = a * b;
 return local;
 }
 
-int main() {
-  printf("The answer is: %d\n", sum(40, 2));
-
-  printf("The alternative answer is: %d\n", multiply(2, 21));
-   return 0;
-}
 int main() {
   printf("The answer is: %d\n", sum(40, 2));
 
